15cb34aff9719f6680b739ef7ad9d1f621913f0c
Q3421Serhat <extoche.398896@gmail.com>
2023-07-28 10:32:45 +0200

    first commit

---
+++ rc04-horoscope-app/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.scss';
+
+const Header = () => {
+    return(
+        <div className="header">
+<span className="text-shadow">who are you?</span>
+        </div>
+    );
+};
+export default Header;
+
